35bde8119731934a7e0bcc98c9aed85bf7ea1c3f
Felipe53 <felipeandresb97@gmail.com>
2022-10-26 14:57:59 -0300

    adds findById to post repository interface

--- src/repositories/IPostRepository.ts
+++ src/repositories/IPostRepository.ts
@@ -1,17 +1,9 @@
 import { Category } from '../entities/Category/Category'
 import { Post } from '../entities/Post'
-
-export interface UpdatePostData {
-  title?: string
-  body?: string
-  excerpt?: string
-  categories?: Category[]
-  ogImageUrl?: string
-}
-
 export interface IPostRepository {
   create: (post: Post) => Promise<Post>
   findAllByAuthorId: (authorId: string) => Promise<Post[]>   
   findBySlug: (slug: string) => Promise<Post | null>
-  update: (id: string, data: UpdatePostData) => Promise<Post>
+  findById: (id: string) => Promise<Post | null>
+  update: (id: string, data: Post) => Promise<Post>
 }
