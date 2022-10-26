737891df13b7205c9cc95a8e1a1e8cb2f1642f16
Felipe53 <felipeandresb97@gmail.com>
2022-10-26 14:59:42 -0300

    adds UpdatePost use-case

--- src/use-cases/UpdatePost/UpdatePost.ts
+++ src/use-cases/UpdatePost/UpdatePost.ts
@@ -1,12 +1,28 @@
-import { IPostRepository, UpdatePostData } from "../../repositories/IPostRepository";
+import { Category } from "../../entities/Category/Category";
+import { Post } from "../../entities/Post";
+import { IPostRepository } from "../../repositories/IPostRepository";
+
+export interface UpdatePostData {
+  title?: string
+  body?: string
+  excerpt?: string
+  categories?: Category[]
+  ogImageUrl?: string
+}
 
 export class UpdatePost {
   constructor(
     private postRepository: IPostRepository
   ) {}
 
-  async execute(id: string, data: UpdatePostData) {
-    const post = await this.postRepository.update(id, data)
+  async execute(id: string, data: UpdatePostData): Promise<Post> {
+    const origialPostData = await this.postRepository.findById(id)
+    if (!origialPostData) throw Error(`Post with id ${id} not found`)
+    const post = new Post(origialPostData)
+    for (const prop of Object.keys(data)) {
+      post[prop] = data[prop]
+    }
+    this.postRepository.update(id, post)
     return post
   }
 }
