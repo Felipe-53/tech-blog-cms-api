3d53b8a0b38bbbb15228b91a92d19bb92f884145
Felipe53 <felipeandresb97@gmail.com>
2022-11-06 12:45:01 -0300

    updates create post use-case to only accept new posts from authors that exists in the repository

--- src/use-cases/Post/CreatePost/CreatePost.ts
+++ src/use-cases/Post/CreatePost/CreatePost.ts
@@ -1,13 +1,18 @@
 import { CreatePostDTO } from "../../../dtos/CreatePostDTO";
 import { Post } from "../../../entities/Post";
+import { IAuthorRepository } from "../../../repositories/IAuthorRepository";
 import { IPostRepository } from "../../../repositories/IPostRepository";
 
 export class CreatePost {
   constructor(
-    private postRepository: IPostRepository
+    private postRepository: IPostRepository,
+    private authorRepository: IAuthorRepository
   ) {}
 
   async execute(data: CreatePostDTO) {
+    const authorId = data.author.id as string
+    const author = await this.authorRepository.findById(authorId)
+    if (!author) throw Error(`No such author with id ${authorId}`)
     const post = new Post(data)
     await this.postRepository.create(post)
     return post
