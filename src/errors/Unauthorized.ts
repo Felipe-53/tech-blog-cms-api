e6f34217545ae5d3efd947b393d0a912ae3973de
Felipe53 <felipeandresb97@gmail.com>
2022-12-06 00:03:29 -0300

    makes massage parameter optional

--- src/errors/Unauthorized.ts
+++ src/errors/Unauthorized.ts
@@ -1,7 +1,11 @@
 import { HTTPError } from "./HTTPError"
 
 export class Unauthorized extends HTTPError {
-  constructor(message: string) {
+  constructor(message?: string) {
+    if (!message) {
+      message = "Unauthorized"
+    }
+
     super({
       code: 401,
       message,
