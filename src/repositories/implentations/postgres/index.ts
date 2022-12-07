c6aa25d39ebcdf85b9f241660afbd76b0be3d00f
Felipe53 <felipeandresb97@gmail.com>
2022-12-07 11:53:49 -0300

    asserts that a test database is used when running tests

--- src/repositories/implentations/postgres/index.ts
+++ src/repositories/implentations/postgres/index.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from "@prisma/client"
+import env from "../../../env"
+
+const databaseUrl = env.database_url
+const testDatabaseName = "testing"
+
+if (env.node_env === "test" && !databaseUrl.includes("testing")) {
+  console.error(
+    `Trying to run tests in a database not called ${testDatabaseName}`
+  )
+  process.exit(1)
+}
 
 const prisma = new PrismaClient()
 
