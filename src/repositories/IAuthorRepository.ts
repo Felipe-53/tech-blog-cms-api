96bfb1f4d518976b71af1668a72c92ad36582631
Felipe53 <felipeandresb97@gmail.com>
2022-12-08 12:53:37 -0300

    modifies author repo interface

--- src/repositories/IAuthorRepository.ts
+++ src/repositories/IAuthorRepository.ts
@@ -1,15 +1,17 @@
+import { DBAuthor } from "@prisma/client"
 import { Author } from "../entities/Author/"
 
 export interface CreateAuthorData {
   name: string
   email: string
-  password: string
+  passwordHash: string
   admin: boolean
 }
 
 export interface IAuthorRepository {
   create: (author: CreateAuthorData) => Promise<Author>
   findById: (id: string) => Promise<Author | null>
-  findByEmail: (email: string) => Promise<Author | null>
+  // TODO: DBAuthor here is a quick-fix
+  findByEmail: (email: string) => Promise<DBAuthor | null>
   delete: (id: string) => Promise<void>
 }
