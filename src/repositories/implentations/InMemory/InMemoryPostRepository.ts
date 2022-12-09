b33977cb95f1f640e5382dcf8989b42b351312c6
Felipe53 <felipeandresb97@gmail.com>
2022-12-09 19:15:34 -0300

    hotfix

--- src/repositories/implentations/InMemory/InMemoryPostRepository.ts
+++ src/repositories/implentations/InMemory/InMemoryPostRepository.ts
@@ -1,6 +1,6 @@
-import { Post } from '../../../entities/Post'
-import { IPostRepository } from '../../../repositories/IPostRepository';
-import data from './inMemoryData';
+import { Post } from "../../../entities/Post"
+import { IPostRepository } from "../../../repositories/IPostRepository"
+import data from "./inMemoryData"
 
 export class InMemoryPostRespository implements IPostRepository {
   async create(post: Post) {
@@ -9,27 +9,28 @@ export class InMemoryPostRespository implements IPostRepository {
   }
 
   async findAllByAuthorId(authorId: string) {
-    const posts = data.posts.filter(post => post.author.id === authorId)
+    const posts = data.posts.filter((post) => post.author.id === authorId)
     return posts
   }
 
   async findBySlug(slug: string) {
-    const post = data.posts.find(post => post.slug === slug)
+    const post = data.posts.find((post) => post.slug === slug)
     if (!post) return null
     return post
   }
 
   async findById(id: string) {
-    const post = data.posts.find(post => post.id === id)
+    const post = data.posts.find((post) => post.id === id)
     if (!post) return null
     return post
   }
 
   async update(id: string, updatedPostData: Post) {
-    const post = data.posts.find(post => post.id === id)
+    const post = data.posts.find((post) => post.id === id)
     if (!post) throw Error(`Could not find post with id ${id}`)
-    for (const prop of Object.keys(updatedPostData)) {
+    for (const prop of Object.keys(updatedPostData) as Array<keyof Post>) {
       if (updatedPostData[prop]) {
+        // @ts-ignore
         post[prop] = updatedPostData[prop]
       }
     }
@@ -38,7 +39,7 @@ export class InMemoryPostRespository implements IPostRepository {
 
   async delete(id: string) {
     let previousLegth = data.posts.length
-    data.posts = data.posts.filter(post => post.id !== id)
+    data.posts = data.posts.filter((post) => post.id !== id)
     let newLength = data.posts.length
     if (previousLegth === newLength) {
       throw Error(`Trying to delete unexisting post with id ${id}`)
