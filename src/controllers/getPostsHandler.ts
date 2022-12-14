8089964a93647a6683e909800b84285ae4af2102
Felipe53 <felipeandresb97@gmail.com>
2022-12-14 12:31:15 -0300

    separate controller for getting all posts

---
+++ src/controllers/getPostsHandler.ts
@@ -0,0 +1,14 @@
+import { FastifyRequest } from "fastify"
+import { PgPostRespository } from "../repositories/implentations/postgres/PgPostRepository"
+import { FindAllPosts } from "../use-cases/Post/FindAllPosts/FindAllPosts"
+
+async function getPostsHandler(req: FastifyRequest) {
+  // @ts-ignore
+  const authorId = req.user.id
+  const postRepo = new PgPostRespository()
+  const findAllPosts = new FindAllPosts(postRepo)
+  const posts = await findAllPosts.execute(authorId)
+  return posts
+}
+
+export { getPostsHandler }
